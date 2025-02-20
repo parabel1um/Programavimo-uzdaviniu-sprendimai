493bc7c6c4ec5177b269c2af227c84b046e58a6d
parabel1um <167568865+parabel1um@users.noreply.github.com>
2025-02-20 23:41:08 +0200

    Lifeguards su paaiškinimais

--- usaco.org/Lifeguards/Lifeguards.cpp
+++ usaco.org/Lifeguards/Lifeguards.cpp
@@ -1,32 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-struct Karv {
-	int start, end;
-};
-
 int main() {
 	freopen("lifeguards.in", "r", stdin);
     freopen("lifeguards.out", "w", stdout);
 	int n;cin>>n;
-	vector<Karv> M(n);
+	vector<pair<int, int>> M(n);
 	int max_time=0;
-	for (Karv &a : M) {
-		cin>>a.start>>a.end;
-		max_time = max(max_time, a.end);
+	for (auto& a : M) {
+		cin>>a.first>>a.second;
+		max_time = max(max_time, a.second);
 	}
 
-	/*
-	Bandome kiekvieną atleisti, ir apskaičiuoti likusį kitų karvių laiką
-	 */
 	int max_cover = 0;
 	for (int i = 0; i < n; i++) {
 		int covered = 0;
-		for (int t = 1; t <= max_time; t++) {
-			for (int j = 0; j < n; j++) {
-				if (j != i) {
-					if (M[j].start <= t && t < M[j].end) {
-						covered++;
+		for (int t = 1; t <= max_time; t++) { // pereiname per didziausia galima laika
+			for (int j = 0; j < n; j++) { // pereiname per visas karves ir tikriname ar musu isrinktoji nera atmesta
+				if (j != i) { // nera atmesta karve
+					if (M[j].first <= t && t < M[j].second) { // tikriname ar t yra kurios nors kitos karves intervale 
+						covered++; // pridedame 1 prie viso galimo laiko, laikai nebutinai turi eiti paeiliui, gali buti tarpas tarp ju
 						break;
 					}
 				}
